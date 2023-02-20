1c5f9ba1e53a58c22cb10906392f6f1648f39f5c
Jakub Sowa <jakub.sowa5@gmail.com>
2023-02-20 14:15:05 +0100

    feat: working pybluez bluetooth connector

--- utils/bluetooth/pybluez.py
+++ utils/bluetooth/pybluez.py
@@ -1,13 +1,30 @@
+import time
+
+import bluetooth
+
+
 class Bluetooth:
-    def __init__(self, mac='00:18:e4:34:d4:18', port=1):
-        pass
+    def __init__(self, mac='20:16:03:10:03:48', port=1):
+        # print(bluetooth.discover_devices())
+        self._socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+        self._socket.connect((mac, port))
 
     def send(self, data):
-        pass
+        self._socket.send(bytes(str(data), 'UTF-8'))
 
     def close_connection(self):
-        pass
+        self._socket.close()
 
     # TODO: if we figure out how to do bluetooth send/receive, change this
     def wait_until_received(self):
         pass
+
+if __name__ == '__main__':
+    # simple inquiry example
+    b = Bluetooth()
+    time.sleep(1)
+    b.send(0)
+    time.sleep(2)
+    b.send(256)
+    # time.sleep(2)
+    # b.close_connection()
