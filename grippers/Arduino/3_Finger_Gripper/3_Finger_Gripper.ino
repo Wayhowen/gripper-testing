bba25af7c5f4ad19f12b511dec33b8cf673a0ccc
Osman Abdinasir Hassan <osha@itu.dk>
2023-02-22 14:33:38 +0100

    Create 3_Finger_Gripper.ino

---
+++ grippers/Arduino/3_Finger_Gripper/3_Finger_Gripper.ino
@@ -0,0 +1,68 @@
+#include <SoftwareSerial.h>
+#include<Servo.h>
+
+SoftwareSerial btSerial(3, 4);
+Servo myservo;
+int pos = 0;
+int sensorPin = A2;
+int sensorValue;
+int threshold = 80;
+int threshCount = 0;
+int gripperDelay = 25;
+int baseGripPosition = 100;
+
+void setup() {
+  myservo.attach(7);
+  Serial.begin(9600);
+  Serial.println("Ready");
+  btSerial.begin(9600);
+  pinMode(INPUT, A2);
+}
+
+void loop() {
+  if (btSerial.available()>0) {
+    String action = btSerial.readString();
+    if(action == "1") engage();
+    else if(action == "0") disengage();
+  }
+
+  if (Serial.available()) btSerial.write(Serial.read());
+}
+
+void engage(){
+  Serial.println("Engaging payload");
+  for (pos = baseGripPosition; pos <= 180; pos += 1) {
+    boolean res = pressureSense();
+    if(res) break;
+    myservo.write(pos);
+    delay(gripperDelay);
+  }
+}
+
+void disengage(){
+  Serial.println("Disengaging payload");
+  myservo.write(baseGripPosition);
+}
+
+boolean pressureSense(){
+  sensorValue = analogRead(sensorPin);
+  /*if(sensorValue > 0){
+    Serial.println(sensorValue);
+    delay(5);
+  }*/
+  if(sensorValue > threshold){
+      threshCount++;
+      Serial.println(threshCount);
+      delay(5);
+      if(threshCount > 2){
+        int stopPos = myservo.read();
+        myservo.write(stopPos);
+        Serial.println("CONTACT");
+        delay(gripperDelay);
+        return true;
+      }
+  } else{
+      threshCount = 0;
+  }
+  return false;
+}
